716a880b3df45330346ae6a2ad159659396e688e
yanmoenaing118 <yan.webdev.1234@gmail.com>
2020-09-23 16:36:24 +0630

    initial version

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose
   })
   .then(() => console.log("DB connection successful!"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
